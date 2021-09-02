de5c193b59c07228b155436a33cb5bf6bee3ba02
khandakar akash <akash.cse10@gmail.com>
2021-09-02 16:21:36 +0600

    DepartementsController created and also create five endpoints with static response.

---
+++ API/Controllers/DepartmentsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("get all departments.");
+        }
+
+        [HttpGet("{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok("get this " + code + " department data.");
+        }
+
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Ok("Insert a new department.");
+        }
+
+        [HttpPut("{code}")]
+        public IActionResult Update(string code)
+        {
+            return Ok("Update this " + code + " department data.");
+        }
+
+        [HttpDelete("{code}")]
+        public IActionResult Delete(string code)
+        {
+            return Ok("Delete this " + code + " department data.");
+        }
+    }
+}
