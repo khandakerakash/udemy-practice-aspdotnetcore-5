462c266dcb9600d78dad575e8599f3f1d13ecb83
khandakar akash <akash.cse10@gmail.com>
2021-09-30 13:43:59 +0600

    UnitOfWork is added in DLL.

---
+++ UdemyPractice/src/DLL/UoW/IUnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using DLL.ApplicationDbContext;
+
+namespace DLL.UoW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<bool> Commit();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
