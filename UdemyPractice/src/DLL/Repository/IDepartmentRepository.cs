58d9d7a131d12e7ebf17ab70a3695369a3da9571
khandakar akash <akash.cse10@gmail.com>
2021-09-30 15:04:51 +0600

    IDepartmentRepository and IStudentRepository are added in DLL.

---
+++ UdemyPractice/src/DLL/Repository/IDepartmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using DLL.ApplicationDbContext;
+using DLL.Model;
+
+namespace DLL.Repository
+{
+    public interface IDepartmentRepository : IRepositoryBase<Department>
+    {
+        
+    }
+
+    public class DepartmentRepository : RepositoryBase<Department>, IDepartmentRepository
+    {
+        public DepartmentRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ UdemyPractice/src/DLL/Repository/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using DLL.ApplicationDbContext;
+using DLL.Model;
+
+namespace DLL.Repository
+{
+    public interface IStudentRepository : IRepositoryBase<Student>
+    {
+        
+    }
+
+    public class StudentRepository : RepositoryBase<Student>, IStudentRepository
+    {
+        public StudentRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
