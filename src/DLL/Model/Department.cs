0e7c03350dbe822676a5b160afbefb4e3c996fe5
khandakerakash <akash.cse10@gmail.com>
2021-10-01 22:57:15 +0600

    Directory change...

--- src/DLL/Model/Department.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DLL.Model
-{
-    public class Department
-    {
-        public int DepartmentId { get; set; }
-        public string Name { get; set; }
-        public string Code { get; set; }
-    }
-}
